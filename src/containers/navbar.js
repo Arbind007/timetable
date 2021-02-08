143f313d3b22807b5fb34e94690013d81c6f942b
Arbind007 <almbbsr456@gmail.com>
2021-02-08 17:24:44 +0530

    styling and navbar

---
+++ src/containers/navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function navbar(props) {
+  return (
+    <div>
+      <nav class="navbar navbar-light" style={{ backgroundColor: "#e3f2fd" }}>
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Time-Table
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default navbar;
